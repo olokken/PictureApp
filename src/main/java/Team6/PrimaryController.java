b882f3ca849e19159217a4f81519d5e95ee56fc8
Ole Lokken <ole.lokken1@gmail.com>
2020-03-23 10:31:40 +0100

    Creating Listview in primarycontroller

--- src/main/java/Team6/PrimaryController.java
+++ src/main/java/Team6/PrimaryController.java
@@ -1,12 +1,30 @@
 package Team6;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import entities.Album;
+import entities.PictureInfo;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
 
 public class PrimaryController {
+    ObservableList<Album> list;
+    ListView<Album> albumList;
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    public void fyllListe() {
+        ArrayList<Album> a = new ArrayList<Album>();
+        Album b = new Album("Sommer");
+        Album c = new Album("Sommer");
+        Album d = new Album("Sommer");
+        Album e = new Album("Sommer");
+        Collections.addAll(a,b,c,d,e);
+        list = FXCollections.observableArrayList(a);
+        albumList.getItems().addAll(list);
     }
+
 }
