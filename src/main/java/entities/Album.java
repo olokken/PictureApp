c3ae6d2f9ad50397006537f75913fb8d95c47e97
Lukas-Stjernen <lostjern@stud.ntnu.no>
2020-03-23 12:50:35 +0100

    added javadoc to Album, User, and PictureInfo.

--- src/main/java/entities/Album.java
+++ src/main/java/entities/Album.java
@@ -1,26 +1,48 @@
+/**
+ * Class to represent a photoalbum
+ *
+ * @author team6
+ */
 package entities;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
-public class Album { //comment
+public class Album {
     private String name;
     private List<PictureInfo> pictures;
 
+    /**
+     * Constructor for an album object
+     * @param name
+     */
     public Album(String name) {
         this.name = name;
         this.pictures = new ArrayList<PictureInfo>();
     }
 
+    /**
+     * method to get the name of the album
+     * @return the name of the album
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * method to get picures in the album
+     * @return returns a list of the pictures in the album
+     */
     public List<PictureInfo> getPictures() {
         return pictures;
     }
 
+    /**
+     * Method to check if a photo is registered in the album
+     * @param filePath the filepath to the picture that is being checked
+     * @return true if registered, false if not
+     */
     private boolean isRegistered(String filePath) {
         Optional<PictureInfo> o = pictures.stream().filter(x -> x.getInterestingMetadata().getFilepath().equals(filePath)).findFirst();
         if(o.isPresent()) {
@@ -29,6 +51,10 @@ public class Album { //comment
         return false;
     }
 
+    /**
+     * Method to add a picture to the album, only if it isnt already
+     * @param filePath filepath of the image to be added
+     */
     public void addPicture(String filePath) {
         if(isRegistered(filePath)) {
             throw new IllegalArgumentException("Bildet er allerede registert");
@@ -37,6 +63,10 @@ public class Album { //comment
         this.pictures.add(p);
     }
 
+    /**
+     * Method to remove image from the album
+     * @param filePath filepath of the image to be removed
+     */
     public void removePicture(String filePath) {
         Optional<PictureInfo> o = pictures.stream().filter(x -> x.getInterestingMetadata().getFilepath().equals(filePath)).findFirst();
         if (o.isPresent()) {
@@ -44,34 +74,59 @@ public class Album { //comment
         }
     }
 
+    /**
+     * method to remove a picture
+     * @param p picture to be removed
+     */
     public void removePicture(PictureInfo p) {
         pictures.remove(p);
     }
 
+    /**
+     * sorts the images in the album based on the ISO data
+     */
     public void sortIso() {
         pictures.sort(Comparator.comparing(x -> x.getInterestingMetadata().getISO()));
     }
 
+    /**
+     * sorts the images in the album based on the ISO data in the opposite order
+     */
     public void sortIsoReversed() {
         pictures.sort(Comparator.comparing(x -> x.getInterestingMetadata().getISO()));
     }
 
+    /**
+     * sorts the images in the album based on the date the photo was taken
+     */
     public void sortDate() {
         pictures.sort(Comparator.comparing(x -> x.getInterestingMetadata().getDateTime()));
     }
 
+    /**
+     * sorts the images in the album based on the exposure time
+     */
     public void sortExposureTime() {
         pictures.sort(Comparator.comparing(x -> x.getInterestingMetadata().getExposureTime()));
     }
 
+    /**
+     * sorts the images in the album based on the file size
+     */
     public void sortFileSize() {
         pictures.sort(Comparator.comparing(x -> x.getInterestingMetadata().getFileSize()));
     }
 
+    /**
+     * sorts the images in the album based on file name
+     */
     public void sortFileName() {
         pictures.sort(Comparator.comparing(x -> x.getInterestingMetadata().getFileName()));
     }
 
+    /**
+     * sorts the images in the album based on whether flash was used
+     */
     public void sortFlashUsed() {
         pictures.sort(Comparator.comparing(x -> x.getInterestingMetadata().isFlashUsed()));
     }
--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -2,11 +2,21 @@ package entities;
 
 import java.util.ArrayList;
 
+/**
+ * class to represent a user
+ *
+ * @author team6
+ */
 public class User {
     private String profileName;
     private String password;
     private ArrayList<Album> albums;
 
+    /**
+     * constructor for the user class
+     * @param profileName name of the user
+     * @param password the users password
+     */
     public User(String profileName, String password) {
         this.profileName = profileName;
         this.password = password;
